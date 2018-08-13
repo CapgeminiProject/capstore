193a516a7400f6516c8008296f0b071350dbde4c
Anish Garg <anish.garg2297@gmail.com>
2018-08-13 16:08:03 +0530

    skeleton of project

---
+++ src/main/java/com/capgemini/store/boot/CapStoreApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.store.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapStoreApplication.class, args);
+	}
+}
